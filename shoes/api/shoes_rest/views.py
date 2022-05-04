fb466bd4c3936a3dff714161ed3df9cd5df578ac
Rachael Schlosberg <rachael.schlosberg@gmail.com>
2022-05-04 17:37:18 -0500

    Made shoe model, made view, configured urls, included app url in project urls, saw empty list in insomnia

--- shoes/api/shoes_rest/views.py
+++ shoes/api/shoes_rest/views.py
@@ -1,3 +1,29 @@
+from django.http import JsonResponse
+from django.views.decorators.http import require_http_methods
+import json
 from django.shortcuts import render
+from common.json import ModelEncoder
+from .models import Shoe
 
 # Create your views here.
+#making encoders to use in view functions
+
+class ShoeListEncoder(ModelEncoder):
+    model = Shoe
+    properties = ["model", "color"]
+
+class ShoeDetailEncoder(ModelEncoder):
+    model = Shoe
+    properties = [
+        "model",
+        "manufacturer",
+        "color",
+        "picture_url",
+    ]
+
+def api_list_shoes(request):
+    shoes = Shoe.objects.all()
+    return JsonResponse(
+        {"shoes": shoes}, 
+        encoder=ShoeListEncoder
+    )
